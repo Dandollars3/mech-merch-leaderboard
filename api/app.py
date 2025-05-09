3fffc8596090331841f1c1574dc7dc0614bfa2dd
Dandollars3 <105294705+Dandollars3@users.noreply.github.com>
2025-05-09 06:54:43 +0100

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,22 +1,28 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, request
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../static", template_folder="../templates")
 
 @app.route('/')
 def home():
-    return send_from_directory('../templates', 'index.html')
+    return send_from_directory(app.template_folder, 'index.html')
 
 @app.route('/static/<path:filename>')
 def serve_static(filename):
-    return send_from_directory('../static', filename)
+    return send_from_directory(app.static_folder, filename)
 
 def handler(request):
-    path = request.path
-    if path.startswith('/static/'):
-        return serve_static(path.split('/static/')[1])
-    return {
-        "statusCode": 200,
-        "headers": {"Content-Type": "text/html"},
-        "body": home().get_data().decode('utf-8')
-    }
+    with app.app_context():
+        path = request.path
+        if path.startswith('/static/'):
+            return serve_static(path.split('/static/')[1])
+        response = home()
+        return {
+            "statusCode": 200,
+            "headers": {"Content-Type": "text/html"},
+            "body": response.get_data().decode('utf-8')
+        }
+
+# For local testing
+if __name__ == '__main__':
+    app.run(port=3000)
